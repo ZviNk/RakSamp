3a7de98f37b3f29df10d930f7a65c086942ea9dd
ZviNk <32645332+ZviNk@users.noreply.github.com>
2025-03-18 16:54:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,117 @@
 /* IMPORTING LIBRARIES: */
 require('dotenv').config(); /* FOR USING .env AS CONFIG */
-const {GatewayIntentBits, Client, Collection, MessageMentions} = require("discord.js"); /* FOR USING discord.js */
-const {readdirSync} = require("node:fs"); /* FOR READING DIRECTORIES */
+const {exec} = require('node:child_process');
+const {db} = require("./db");
+const {readdirSync, unlinkSync, existsSync, mkdirSync} = require("fs");
+const {join, resolve} = require("path");
 
-/* LOCALES FUNCTIONS: */
-const locales = {};
-readdirSync('./locales').forEach(file => {
-    const localeCode = file.replace(/\.json$/, '');
-    const localePath = `./locales/${file}`;
-    locales[localeCode] = require(localePath);
-});
+main();
+setInterval(checkTasks, 5000);
+setInterval(checkLogs, 60 * 60 * 1000);
 
-function getLocale(locale, string, replacements = {}) {
-    return locales[locale][string].replace(/{{\s*(\w+)\s*}}/g, (match, variable) => {
-        return replacements[variable] || match;
-    });
+async function main() {
+    const servers = await db(`SELECT * FROM \`arizona\`.servers_raksamp WHERE \`active\` = true AND \`start\` = true;`);
+
+    for (const server of servers) {
+        await createLogs(server.server_id);
+        console.log(`Executing: wine raksamp/arizona.exe -project 1 -server ${server.server_id}`);
+
+        try {
+            const { stdout, stderr } = await exec(`wine raksamp/arizona.exe -project 1 -server ${server.server_id}`);
+            console.log("Command executed successfully.");
+            console.log("stdout:", stdout);
+            console.error("stderr:", stderr);
+        } catch (error) {
+            console.error("Error executing command:", error);
+        }
+        console.log(`[ RakSamp ] Started ${server.server_name} [IP: ${server.server_ip} | PORT: ${server.server_port}].`);
+    }
 }
 
-/* DISCORD FUNCTIONS: */
-const bot = new Client({
-    intents: [
-        GatewayIntentBits.DirectMessageReactions,
-        GatewayIntentBits.GuildBans,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.GuildScheduledEvents,
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.DirectMessageTyping,
-        GatewayIntentBits.GuildEmojisAndStickers,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.GuildIntegrations,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildWebhooks,
-    ],
-    allowedMentions: {
-        parse: [
-            'users',
-            'roles',
-            'everyone',
-        ],
-        repliedUser: true
-    },
-    partials: [
-        'MESSAGE',
-        'CHANNEL',
-        'REACTION',
-        'USER',
-        'GUILD_MEMBER'
-    ]
-});
+async function checkTasks() {
+    const tasks = await db(`SELECT * FROM \`arizona\`.tasks_system WHERE \`passed\` = false;`);
 
-bot.commands = new Collection();
+    for (const task of tasks) {
+        const data = await db(`SELECT * FROM \`arizona\`.servers_raksamp WHERE \`server_id\` = ${task.server_id};`);
+        const server = data[0];
+        const time_created = new Date(task.time_created + ' UTC');
+        const time_half_passed = new Date(task.time_half_passed + ' UTC');
+        const time_passed = new Date(task.time_passed + ' UTC');
 
-bot.login(Number(process.env.STATUS) === 0 ? process.env.DISCORD_TOKEN_DEV : process.env.DISCORD_TOKEN_RELEASE);
+        if (!server.active) {
+            await db(`UPDATE \`arizona\`.tasks_system SET \`error\` = true, \`text_error\` = "Система отключена на данном сервере." WHERE \`server_id\` = ${task.server_id};`);
+            continue;
+        }
 
-readdirSync('./handlers').forEach(file => {
-    require(`./handlers/${file}`)(bot);
-});
+        switch (task.task) {
+            case 'restart':
+                if (new Date() - time_created >= 10000) {
+                    await db(`UPDATE \`arizona\`.tasks_system SET \`error\` = true, \`text_error\` = "Сервер выключен.", \`half_passed\` = true, \`time_half_passed\` = DEFAULT, \`passed\` = true, \`time_passed\` = DEFAULT WHERE \`server_id\` = ${server.server_id} AND \`task_id\` = ${task.task_id};`);
+                    break;
+                }
+                if (!task.half_passed) break;
 
-/* EXPORTING FUNCTIONS: */
-module.exports = {getLocale};
+                await db(`UPDATE \`arizona\`.tasks_system SET \`passed\` = true WHERE \`server_id\` = ${server.server_id};`);
+                await exec(`wine raksamp/arizona.exe -project 1 -server ${server.server_id}`);
+                console.log(`[ RakSamp ] Restarted ${server.server_name} [IP: ${server.server_ip} | PORT: ${server.server_port}].`);
+                break;
+            case 'start':
+                if (server.started) {
+                    await db(`UPDATE \`arizona\`.tasks_system SET \`error\` = true, \`text_error\` = "Сервер уже запущен.", \`half_passed\` = true, \`time_half_passed\` = DEFAULT, \`passed\` = true, \`time_passed\` = DEFAULT WHERE \`server_id\` = ${server.server_id} AND \`task_id\` = ${task.task_id};`);
+                    break;
+                }
+
+                await db(`UPDATE \`arizona\`.servers_raksamp SET \`start\` = true WHERE \`server_id\` = ${server.server_id};`);
+                await db(`UPDATE \`arizona\`.tasks_system SET \`half_passed\` = true, \`time_half_passed\` = DEFAULT, \`passed\` = true, \`time_passed\` = DEFAULT WHERE \`server_id\` = ${server.server_id};`);
+                await exec(`wine raksamp/arizona.exe -project 1 -server ${server.server_id}`);
+                console.log(`[ RakSamp ] Started ${server.server_name} [IP: ${server.server_ip} | PORT: ${server.server_port}].`);
+                break;
+            case 'stop':
+                if (new Date() - time_created >= 10000) {
+                    await db(`UPDATE \`arizona\`.tasks_system SET \`error\` = true, \`text_error\` = "Сервер уже выключен.", \`half_passed\` = true, \`time_half_passed\` = DEFAULT, \`passed\` = true, \`time_passed\` = DEFAULT WHERE \`server_id\` = ${server.server_id} AND \`task_id\` = ${task.task_id};`);
+                    break;
+                }
+                if (!task.half_passed) break;
+
+                await db(`UPDATE \`arizona\`.tasks_system SET \`passed\` = true WHERE \`server_id\` = ${server.server_id};`);
+                console.log(`[ RakSamp ] Stopped ${server.server_name} [IP: ${server.server_ip} | PORT: ${server.server_port}].`);
+            default:
+                break;
+        }
+    }
+}
+
+async function checkLogs() {
+    const sixMonthsAgo = new Date();
+    sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+    const servers = readdirSync("./logs");
+
+    for (const server of servers) {
+        const serverLogsDir = join("./logs", server);
+        const logs = readdirSync(`./logs/${server}`);
+
+        for (const log of logs) {
+            const logPath = join(serverLogsDir, log);
+
+            const match = log.match(/^(\d{2})\.(\d{2})\.(\d{4})\.log$/);
+            if (match) {
+                const [_, day, month, year] = match;
+                const logDate = new Date(`${year}-${month}-${day}`);
+
+                if (logDate < sixMonthsAgo) {
+                    unlinkSync(logPath);
+                    console.log(`[ Logs ] Deleted "${log}" from server №${server}.`);
+                }
+            }
+        }
+    }
+}
+
+async function createLogs(id) {
+    const logsPath = resolve(`${process.env.PATH_MAIN}/logs/${id}`);
+
+    if (!existsSync(logsPath)) {
+        mkdirSync(logsPath, { recursive: true });
+        console.log(`[ Logs ] Created server №${id} logs directory.`);
+    }
+}
