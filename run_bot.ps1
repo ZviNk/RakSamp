70a01c9f7d9dd0a354b2d871ad861bb3f68aec16
ZviNk <32645332+ZviNk@users.noreply.github.com>
2025-03-27 16:53:39 +0300

    Create run_bot.ps1

---
+++ run_bot.ps1
@@ -0,0 +1,21 @@
+# Путь до exe-файла
+$exePath = "/app/raksamp/arizona.exe"
+
+if (-Not (Test-Path $exePath)) {
+    Write-Host "Файл $exePath не найден!"
+    exit 1
+}
+
+try {
+    # Запускаем .exe через Wine в виртуальном X-сервере с помощью xvfb-run
+    Start-Process "xvfb-run" -ArgumentList "wine", $exePath -NoNewWindow
+    Write-Host "Процесс запущен успешно."
+}
+catch {
+    Write-Host "Ошибка при запуске процесса: $_"
+}
+
+# Бесконечное ожидание, чтобы контейнер не завершался
+while ($true) {
+    Start-Sleep -Seconds 3600
+}
